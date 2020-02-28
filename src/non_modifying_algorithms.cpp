a7a97dca602bcad0adefa9e05c8f9ccc31f10f48
Pedro Margarido <margarido@hbm.com>
2020-02-28 14:47:32 +0100

    Changed algorithms example to non_modifying_algorithms

--- src/non_modifying_algorithms.cpp
+++ src/non_modifying_algorithms.cpp
@@ -62,4 +62,37 @@ int main ()
         cout << "At least one value  " << nValue << " is divisable by " << nDivisor << endl;
     }
 
+    // for_each
+    cout << "--- std::for_each ---" << endl;
+    int nComparison = 5;
+    vector<int> vecForEach{2, 4, 6, 8, 10, 12, 14};
+    for_each(vecForEach.cbegin(), vecForEach.cend(), [nComparison](int value){ 
+                if(nComparison > value)
+                    cout << "Value " << value << " is less than " << nComparison << endl;
+       });
+
+    // for_each_n
+    cout << "--- std::for_each_n ---" << endl;
+    for_each_n(vecForEach.cbegin(), 1, [nComparison](int value){ 
+                if(nComparison > value)
+                    cout << "Value " << value << " is less than " << nComparison << endl;
+       });
+
+    // count
+    cout << "--- std::count ---" << endl;
+    vector<int> vecCount{2, 4, 6, 8, 8, 8, 14};
+    int nCount = count(vecCount.cbegin(), vecCount.cend(), 8);
+    cout << nCount << endl;
+
+    // count_if
+    cout << "--- std::count_if ---" << endl;
+    nCount = count_if(vecCount.cbegin(), vecCount.cend(), [](int value){
+                if(value >= 8)
+                    return true;
+                else
+                    return false;
+            });
+
+    cout << nCount << endl;
+
 } 
