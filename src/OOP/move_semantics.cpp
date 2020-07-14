bc6311ca57be3e448409dd593e30812585fb3d1e
Pedro Margarido <margarido@hbm.com>
2020-07-14 17:46:15 +0200

    Added move assignment operator

--- src/OOP/move_semantics.cpp
+++ src/OOP/move_semantics.cpp
@@ -4,57 +4,76 @@
 class String 
 {
     public:
-       String() = default;
+        String() = default;
 
-       String(const char* str)
-       {
-           m_Size = strlen(str);
-           m_Data = new char[m_Size];
+        String(const char* str)
+        {
+            m_Size = strlen(str);
+            m_Data = new char[m_Size];
 
-           std::cout << "Constructing String object " << &m_Data << "\n";
-           memcpy(m_Data, str, m_Size);
-       }
+            std::cout << "Constructing String object " << &m_Data << "\n";
+            memcpy(m_Data, str, m_Size);
+        }
 
-       String(const String& other)
-       {
-           m_Size = other.m_Size;
-           m_Data = new char[m_Size];
+        String(const String& other)
+        {
+            m_Size = other.m_Size;
+            m_Data = new char[m_Size];
 
-           std::cout << "Copying String object " << &m_Data << "\n";
-           memcpy(m_Data, other.m_Data, m_Size);
-       }
+            std::cout << "Copying String object " << &m_Data << "\n";
+            memcpy(m_Data, other.m_Data, m_Size);
+        }
 
-       String(String&& other)
-       {
-           m_Size = other.m_Size;
-           m_Data = other.m_Data;
+        String(String&& other)
+        {
+            m_Size = other.m_Size;
+            m_Data = other.m_Data;
 
-           std::cout << "Moving m_Data from String object " << &other.m_Data << " to " << &m_Data <<  "\n";
+            std::cout << "Moving m_Data from String object " << &other.m_Data << " to " << &m_Data <<  "\n";
 
-           other.m_Size = 0;
-           other.m_Data = nullptr;
-       }
+            other.m_Size = 0;
+            other.m_Data = nullptr;
+        }
 
-       ~String()
-       {
-           std::cout << "Destructing String object " << &m_Data << "\n";
-           delete[] m_Data;
-       }
+        String& operator= (String&& other)
+        {
 
-       void Print()
-       {
-           std::cout << m_Data << "\n";
-       }
+            if (this != &other)
+            {
+                delete[] m_Data;
 
-       char* m_Data;
-       size_t m_Size;
+                m_Size = other.m_Size;
+                m_Data = other.m_Data;
+
+                std::cout << "Moving m_Data from String object by assignment operator " << &other.m_Data << " to " << &m_Data <<  "\n";
+
+                other.m_Size = 0;
+                other.m_Data = nullptr;
+            }
+
+            return *this;
+        }
+
+        ~String()
+        {
+            std::cout << "Destructing String object " << &m_Data << "\n";
+            delete[] m_Data;
+        }
+
+        void Print()
+        {
+            std::cout << m_Data << "\n";
+        }
+
+        char* m_Data;
+        size_t m_Size;
 };
 
 class stringHolder
 {
     public:
-    stringHolder(String&& str):
-        m_strHold(std::move(str))
+        stringHolder(String&& str):
+            m_strHold(std::move(str))
     {
         std::cout << "Constructing stringHolder object \n";
     }
@@ -65,5 +84,18 @@ class stringHolder
 
 int main()
 {
-    stringHolder strHold("Pedro");
-}
+
+    String str("Pedro");
+    String strSara("Sara");
+
+    // move constructor 
+    String dest = std::move(str);
+
+    // move assignment operator
+    dest = std::move(strSara);
+
+    dest = std::move(dest);
+
+    dest.Print();
+
+};
